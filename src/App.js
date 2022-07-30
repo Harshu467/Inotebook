7ec624ed81c3b60fc408f92839f12adb38dbc814
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-30 19:45:17 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import NoteState from './context/notes/NoteState';
+// import Alert from './Components/Alert/Alert';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          {/* <Alert /> */}
+          <div className="container">
+            <Routes>
+              <Route exact path='/' index element={<Home />} />
+              <Route exact path='/about' element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
+    </>
   );
 }
 
