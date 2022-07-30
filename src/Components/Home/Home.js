70c5d73e71bbb3fd4b3c9bb47668ef7160ec8358
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-30 19:44:53 +0530

    Created home

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import NoteItem from '../NoteItem/NoteItem'
+import Notes from '../Notes/Notes'
+const Home = () => {
+    return (
+        <>
+            <Notes />
+        </>
+    )
+}
+
+export default Home
