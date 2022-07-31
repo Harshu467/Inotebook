d3bee8a151b890da4edb92e5ced98f5c2ab340f1
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-31 21:39:12 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar/Navbar';
 import About from './Components/About/About'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NoteState from './context/notes/NoteState';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
 // import Alert from './Components/Alert/Alert';
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
             <Routes>
               <Route exact path='/' index element={<Home />} />
               <Route exact path='/about' element={<About />} />
+              <Route exact path='/login' element={<Login />} />
+              <Route exact path='/signup' element={<Signup />} />
             </Routes>
           </div>
         </BrowserRouter>
